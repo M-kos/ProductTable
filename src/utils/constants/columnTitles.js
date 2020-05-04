e6b6c5e44915d762fe2fbc25fdaf5a2606e257f4
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-05-04 18:46:53 +0300

    constatnt columnTitles

---
+++ src/utils/constants/columnTitles.js
@@ -0,0 +1,32 @@
+export const COLUMN_TITLES = [
+  {
+    value: 'product',
+    title: 'Product (100g serving)',
+    show: true
+  },
+  {
+    value: 'calories',
+    title: 'Calories',
+    show: true
+  },
+  {
+    value: 'fat',
+    title: 'Fat (g)',
+    show: true
+  },
+  {
+    value: 'carbs',
+    title: 'Carbs (g)',
+    show: true
+  },
+  {
+    value: 'protein',
+    title: 'Protein (g)',
+    show: true
+  },
+  {
+    value: 'iron',
+    title: 'Iron (%)',
+    show: true
+  }
+]

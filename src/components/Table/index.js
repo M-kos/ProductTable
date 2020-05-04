84f2b3c432743be8b3c82e9a9f186a4ebe36215c
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-05-04 18:46:32 +0300

    New component Table

---
+++ src/components/Table/index.js
@@ -0,0 +1,34 @@
+import render from './render';
+
+export default {
+  name: 'Table',
+
+  props: {
+    headers: {
+      type: Array,
+      default: () => []
+    },
+
+    items: {
+      type: Array,
+      default: () => []
+    },
+
+    selectable: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    columnTypes() {
+      if (Array.isArray(this.headers) && this.headers.length > 0) {
+        return this.headers.map(header => {
+          return header.value
+        })
+      }
+    }
+  },
+
+  render
+};
---
+++ src/components/Table/render.js
@@ -0,0 +1,87 @@
+function renderCheckBox(h) {
+  if (this.selectable) {
+    return h(
+      'td'
+    )
+  }
+}
+
+function renderTableHead(h) {
+  if (Array.isArray(this.headers) && this.headers.length > 0) {
+    return h(
+      'thead',
+      {
+        class: 'table__head'
+      },
+      [
+        h(
+          'tr',
+          {
+            class: 'table__head-row'
+          },
+          [
+            renderCheckBox.call(this, h),
+
+            ...this.headers.map((header, index) => {
+              return h(
+                'th',
+                {
+                  class: 'table__head-cell',
+                  on: {
+                    click: () => {}
+                  },
+                  key: `${header.value || header.title}-${index}-${this._uid}`
+                },
+                header.title
+              )
+            })
+          ]
+        )
+      ]
+    )
+  }
+}
+
+function renderTableBody(h) {
+  if (Array.isArray(this.items) && this.items.length > 0 && Array.isArray(this.columnTypes)) {
+    return h(
+      'tbody',
+      {
+        class: 'table__body'
+      },
+      this.items.map((item, index) => {
+        return h(
+          'tr',
+          {
+            class: 'table__body-row',
+            key: item.id || `${index}-${this._uid}`
+          },
+          [
+            renderCheckBox.call(this, h),
+
+            ...this.columnTypes.map(columnType => {
+              return h(
+                'td',
+                {
+                  class: 'table__body-cell',
+                  key: `${columnType}-${item.id || index}-${this._uid}`
+                },
+                item[columnType]
+              )
+            })
+          ]
+        )
+      })
+    )
+  }
+}
+
+export default function(h) {
+  return h(
+    'table',
+    {
+      class: 'table'
+    },
+    [ renderTableHead.call(this, h), renderTableBody.call(this, h) ]
+  );
+}

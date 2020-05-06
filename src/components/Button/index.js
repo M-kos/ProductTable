40f473ae6de317b1f738e91f25f69d372104245f
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-05-06 17:46:23 +0300

    Added Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,15 @@
+import render from './render'
+
+export default {
+  name: 'Button',
+
+  functional: true,
+
+  props: {
+    outline: Boolean,
+    loading: Boolean,
+    disabled: Boolean
+  },
+
+  render
+}
---
+++ src/components/Button/render.js
@@ -0,0 +1,48 @@
+import { PulseLoader } from '@saeris/vue-spinners'
+
+function renderPreLoader(h, ctx) {
+  if (ctx.props.loading) {
+    return h(
+      PulseLoader,
+      {
+        props: {
+          size: 5
+        }
+      }
+    )
+  }
+}
+
+export default function(h, ctx) {
+  return h(
+    'button',
+    {
+      attrs: Object.assign(
+        {
+          disabled: ctx.props.disabled
+        },
+        ctx.data.attrs
+      ),
+      class: {
+        button: true,
+        'button--outline': ctx.props.outline,
+        'button--disabled': ctx.props.disabled,
+        'button--loading': ctx.props.loading
+      },
+      style: ctx.data.style,
+      on: ctx.data.on,
+      key: ctx.data.key
+    },
+    [
+      h(
+        'div',
+        {
+          class: 'button__content'
+        },
+        [ ctx.children ]
+      ),
+
+      renderPreLoader.call(this, h, ctx)
+    ]
+  )
+}

8733d91cea2d841866e566157e7f631ddd42bd99
Konstantin Melnikov <koc.melnikov.1985@gmail.com>
2020-05-10 15:13:35 +0300

    Const AmountElements

---
+++ src/utils/constants/amountElements.js
@@ -0,0 +1,14 @@
+export const AMOUNT_ELEMENTS = [
+  {
+    value: 10,
+    title: '10 Per Page'
+  },
+  {
+    value: 25,
+    title: '25 Per Page'
+  },
+  {
+    value: 50,
+    title: '50 Per Page'
+  }
+]
--- src/utils/constants/index.js
+++ src/utils/constants/index.js
@@ -1,1 +1,2 @@
 export * from './columnTitles'
+export * from './amountElements'
